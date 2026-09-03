0cc66499ec59a77111866c49f826db73b15af405
RELover97 <re.forever.lover.97@gmail.com>
2026-09-03 22:07:20 +0300

    2. preprocessor

--- caos/sem1/example/main.cpp
+++ caos/sem1/example/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include "math.h"
 
-int main() 
+#define DEBUG_MESSAGE 1
+
+#if DEBUG_MESSAGE
+#define MESSAGE "debug"
+#else
+#define MESSAGE "release"
+#endif
+
+int main()
 {
     int x = 7;
 
     std::cout << "square = " << square(x) << '\n';
     std::cout << "divide = " << divide(20, 4) << '\n';
+
+    std::cout << MESSAGE << '\n';
 
     return 0;
 }
--- caos/sem1/example/readme.md
+++ caos/sem1/example/readme.md
@@ -9,3 +9,15 @@ Commits:
 `g++ -std=c++20 -v main.cpp math.cpp -o app`
 
 `g++ -std=c++20 -### main.cpp math.cpp -o app`
+
+2. Preprocessor
+
+Добавим директивы в `main.cpp`
+
+`g++ -E main.cpp -o main.i`
+
+Посчитать число строк в файле: 
+
+`wc -l main.cpp`
+
+`wc -l main.i`
